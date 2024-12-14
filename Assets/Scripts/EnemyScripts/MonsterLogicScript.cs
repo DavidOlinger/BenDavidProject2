ecb2c48f2027fab80a7ce4bec25c9cfe01340ffe
Ben Smith <122588740+BenSmith1202@users.noreply.github.com>
2024-12-14 00:01:12 -0500

    donw

--- Assets/Scripts/EnemyScripts/MonsterLogicScript.cs
+++ Assets/Scripts/EnemyScripts/MonsterLogicScript.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using UnityEditor.Build;
 using UnityEngine;
 
 public class MonsterLogicScript : MonoBehaviour
